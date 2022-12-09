39ec05ef8cda8af6a50b8303d60367529c3ccb11
Laith-GLaDOS <usr.bin.ducc.2@gmail.com>
2022-12-09 16:18:26 +0300

    finished JSONObject class

---
+++ src/ziph/JSONObject.java
@@ -0,0 +1,124 @@
+package ziph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class KeyValue {
+  public String key;
+  public String type;
+  public String value;
+
+  public KeyValue(String key, String type, String value) {
+    this.key = key;
+    this.type = type;
+    this.value = value;
+  }
+}
+
+public class JSONObject {
+  private List<KeyValue> jsonData;
+
+  public JSONObject() {
+    this.jsonData = new ArrayList<KeyValue>();
+  }
+
+  private int checkIfSetKeyInsteadOfAddKey(String key) {
+    int setIndex = -1;
+
+    for (int i = 0; i < this.jsonData.size(); i++)
+      if (this.jsonData.get(i).key.equals(key)) {
+        setIndex = i;
+        break;
+      }
+
+    return setIndex;
+  }
+
+  public void set(String key, boolean value) {
+    int setIndex = this.checkIfSetKeyInsteadOfAddKey(key);
+    if (setIndex != -1) {
+      this.jsonData.set(setIndex, new KeyValue(key, "bool", value ? "true" : "false"));
+      return;
+    }
+    this.jsonData.add(new KeyValue(key, "bool", value ? "true" : "false"));
+  }
+  public void set(String key, int value) {
+    int setIndex = this.checkIfSetKeyInsteadOfAddKey(key);
+    if (setIndex != -1) {
+      this.jsonData.set(setIndex, new KeyValue(key, "int", Integer.toString(value)));
+      return;
+    }
+    this.jsonData.add(new KeyValue(key, "int", Integer.toString(value)));
+  }
+  public void set(String key, float value) {
+    int setIndex = this.checkIfSetKeyInsteadOfAddKey(key);
+    if (setIndex != -1) {
+      this.jsonData.set(setIndex, new KeyValue(key, "float", Float.toString(value)));
+      return;
+    }
+    this.jsonData.add(new KeyValue(key, "float", Float.toString(value)));
+  }
+  public void set(String key, double value) {
+    int setIndex = this.checkIfSetKeyInsteadOfAddKey(key);
+    if (setIndex != -1) {
+      this.jsonData.set(setIndex, new KeyValue(key, "double", Double.toString(value)));
+      return;
+    }
+    this.jsonData.add(new KeyValue(key, "double", Double.toString(value)));
+  }
+  public void set(String key, String value) {
+    int setIndex = this.checkIfSetKeyInsteadOfAddKey(key);
+    if (setIndex != -1) {
+      this.jsonData.set(setIndex, new KeyValue(key, "string", value));
+      return;
+    }
+    this.jsonData.add(new KeyValue(key, "string", value));
+  }
+
+  public Object get(String key) {
+    for (int i = 0; i < this.jsonData.size(); i++) {
+      KeyValue currentKeyValue = this.jsonData.get(i);
+      if (currentKeyValue.key.equals(key))
+        switch (currentKeyValue.type) {
+          case "bool":
+            return (currentKeyValue.value.equals("true") ? true : false);
+          case "int":
+            return Integer.parseInt(currentKeyValue.value);
+          case "float":
+            return Float.parseFloat(currentKeyValue.value);
+          case "double":
+            return Double.parseDouble(currentKeyValue.value);
+          case "string":
+            return currentKeyValue.value;
+        }
+    }
+    return null;
+  }
+
+  public List<String> getKeys() {
+    List<String> keys = new ArrayList<String>();
+    for (int i = 0; i < this.jsonData.size(); i++)
+      keys.add(this.jsonData.get(i).key);
+    return keys;
+  }
+
+  public void delete(String key) {
+    for (int i = 0; i < this.jsonData.size(); i++)
+      if (this.jsonData.get(i).key.equals(key)) {
+        this.jsonData.remove(i);
+        return;
+      }
+  }
+
+  public String toString() {
+    String jsonString = "{";
+    for (int i = 0; i < this.jsonData.size(); i++) {
+      KeyValue currentKeyValue = this.jsonData.get(i);
+      String valueAsJsonValue = "";
+      if (currentKeyValue.type.equals("string")) valueAsJsonValue = "\"" + currentKeyValue.value + "\"";
+      else valueAsJsonValue = currentKeyValue.value;
+      jsonString += "\"" + currentKeyValue.key.replaceAll("\"", "\\\"") + "\":" + valueAsJsonValue.replaceAll("\"", "\\\"") + (i != jsonData.size() - 1 ? "," : "");
+    }
+    return jsonString + "}";
+  }
+}
---
+++ src/ziph/JSONObjectFromString.java
@@ -0,0 +1,5 @@
+package ziph;
+
+public class JSONObjectFromString extends JSONObject {
+  
+}
